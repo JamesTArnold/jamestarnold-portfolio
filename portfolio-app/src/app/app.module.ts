e6957be3c9d40126ca6f66b0eca2d54e8f3bf2f3
James T. Arnold <jamestarnold85@gmail.com>
2021-10-21 13:35:16 -0500

    init

---
+++ portfolio-app/src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { environment } from '../environments/environment';
+import { provideAnalytics,getAnalytics,ScreenTrackingService,UserTrackingService } from '@angular/fire/analytics';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAnalytics(() => getAnalytics())
+  ],
+  providers: [
+    ScreenTrackingService,UserTrackingService
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
