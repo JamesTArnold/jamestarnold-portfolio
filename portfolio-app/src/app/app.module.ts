170d63a64cdb9d53e2706a3276e284518a3ed1b5
James T. Arnold <jamestarnold85@gmail.com>
2021-10-21 14:02:27 -0500

    firebase fix

--- portfolio-app/src/app/app.module.ts
+++ portfolio-app/src/app/app.module.ts
@@ -6,6 +6,8 @@ import { AppComponent } from './app.component';
 import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
 import { environment } from '../environments/environment';
 import { provideAnalytics,getAnalytics,ScreenTrackingService,UserTrackingService } from '@angular/fire/analytics';
+import { provideAuth,getAuth } from '@angular/fire/auth';
+import { provideFirestore,getFirestore } from '@angular/fire/firestore';
 
 @NgModule({
   declarations: [
@@ -15,7 +17,9 @@ import { provideAnalytics,getAnalytics,ScreenTrackingService,UserTrackingService
     BrowserModule,
     AppRoutingModule,
     provideFirebaseApp(() => initializeApp(environment.firebase)),
-    provideAnalytics(() => getAnalytics())
+    provideAnalytics(() => getAnalytics()),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore())
   ],
   providers: [
     ScreenTrackingService,UserTrackingService
