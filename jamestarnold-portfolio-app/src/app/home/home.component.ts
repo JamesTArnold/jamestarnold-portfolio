a46f3be079999dad323e754ca9579e0e1d42cee9
James T. Arnold <jamestarnold85@gmail.com>
2021-11-17 22:17:53 -0600

    section 1 and material

--- jamestarnold-portfolio-app/src/app/home/home.component.ts
+++ jamestarnold-portfolio-app/src/app/home/home.component.ts
@@ -14,12 +14,29 @@ export class HomeComponent implements OnInit {
 
     gsap.registerPlugin(TextPlugin);
 
-    gsap.to('.section-1-greeting-text', {
-      duration: 3,
-      text: 'Front End Developer',
-      delay: 2,
-      ease: 'sine',
-    });
+    let tl = gsap.timeline();
+    tl.from('.section-1-title-photo', {
+      duration: 2,
+      opacity: 0,
+      y: 150,
+    })
+      .from('.section-1-greeting-text', {
+        duration: 1,
+        opacity: 0,
+        y: 150,
+      })
+      .to('.section-1-greeting-text', {
+        duration: 1,
+        text: { value: 'Front End Developer', delimiter: ' ' },
+        delay: 1,
+        ease: 'sine',
+      })
+      .from('.section-1-skillset-logo', {
+        duration: 1,
+        opacity: 0,
+        x: 150,
+        stagger: 0.25,
+      });
   }
 
   private initParallax() {
