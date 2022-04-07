a627780e2e8b170ac6fd952dab15760bb4d56bd7
James T. Arnold <jamestarnold85@gmail.com>
2022-04-07 13:58:26 -0500

    firebase deploy and added react-sales

--- jamestarnold-portfolio-app/src/environments/environment.prod.ts
+++ jamestarnold-portfolio-app/src/environments/environment.prod.ts
@@ -1,3 +1,18 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyBv4_D5E3YAXOEy0sfKbU22sQA5HUsDK8I',
+
+    authDomain: 'portfolio-50fe3.firebaseapp.com',
+
+    projectId: 'portfolio-50fe3',
+
+    storageBucket: 'portfolio-50fe3.appspot.com',
+
+    messagingSenderId: '924191905593',
+
+    appId: '1:924191905593:web:d037a4ccf8c838e489bae8',
+
+    measurementId: 'G-MRZSJ1H6GL',
+  },
 };
